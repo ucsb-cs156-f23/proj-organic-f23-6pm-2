088eb93c9f5aade5f57317cc2c978db5569c22a1
Jeffrey Cao <sparklyjeff@gmail.com>
2023-11-26 15:15:43 -0800

    created user tables for frontend

--- frontend/src/main/components/Users/UsersTable.js
+++ frontend/src/main/components/Users/UsersTable.js
@@ -1,5 +1,6 @@
 import React from "react";
-import OurTable from "main/components/OurTable"
+import OurTable, { ButtonColumn } from "main/components/OurTable"
+import { useBackendMutation } from "main/utils/useBackend";
 import { formatTime } from "main/utils/dateUtils";
 
 const columns = [
@@ -29,11 +30,65 @@ const columns = [
         id: 'admin',
         accessor: (row, _rowIndex) => String(row.admin) // hack needed for boolean values to show up
     },
+    {
+        Header: 'instructor',
+        id: 'instructor',
+        accessor: (row, _rowIndex) => String(row.instructor) // hack needed for boolean values to show up
+    },
 ];
 
-export default function UsersTable({ users }) {
-    return <OurTable
-        data={users}
-        columns={columns}
-        testid={"UsersTable"} />;
+export default function UsersTable({ users, showToggleButtons = true}) {
+    function cellToAxiosParamsToggleAdmin(cell) {
+        return {
+            url: "/api/admin/users/toggleAdmin",
+            method: "POST",
+            params: {
+                id: cell.row.values.id
+            }
+        }
+    }
+    
+    const toggleAdminMutation = useBackendMutation(
+        cellToAxiosParamsToggleAdmin,
+        {},
+        ["/api/admin/users"]
+    );
+    
+    const toggleAdminCallback = async (cell) => { toggleAdminMutation.mutate(cell); }
+
+    function cellToAxiosParamsToggleInstructor(cell) {
+        return {
+            url: "/api/admin/users/toggleInstructor",
+            method: "POST",
+            params: {
+                id: cell.row.values.id
+            }
+        }
+    }
+    
+    const toggleInstructorMutation = useBackendMutation(
+        cellToAxiosParamsToggleInstructor,
+        {},
+        ["/api/admin/users"]
+    );
+    
+    const toggleInstructorCallback = async (cell) => { toggleInstructorMutation.mutate(cell); }
+
+    const buttonColumn = [
+        ...columns,
+        ButtonColumn("toggle-admin", "primary", toggleAdminCallback, "UsersTable"),
+        ButtonColumn("toggle-instructor", "primary", toggleInstructorCallback, "UsersTable")
+    ]
+    if (showToggleButtons) {
+        return <OurTable
+            data={users}
+            columns={buttonColumn}
+            testid={"UsersTable"} />;
+    }
+    else{
+        return <OurTable
+            data={users}
+            columns={columns}
+            testid={"UsersTable"} />;
+    }
 };
