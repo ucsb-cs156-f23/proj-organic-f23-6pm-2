2a7b9b3560099e4754f3f31ce2cb3f35a5eb1900
Jeffrey Cao <sparklyjeff@gmail.com>
2023-11-28 14:34:07 -0800

    merged chris-f1

--- frontend/src/main/components/Users/UsersTable.js
+++ frontend/src/main/components/Users/UsersTable.js
@@ -3,42 +3,41 @@ import OurTable, { ButtonColumn } from "main/components/OurTable"
 import { useBackendMutation } from "main/utils/useBackend";
 import { formatTime } from "main/utils/dateUtils";
 
-export default function UsersTable({ users, showToggleButtons = true}) {
-    const columns = [
-        {
-            Header: 'githubId',
-            accessor: 'githubId', // accessor is the "key" in the data
-        },
-        {
-            Header: 'githubLogin',
-            accessor: 'githubLogin', // accessor is the "key" in the data
-        },
-        {
-            Header: 'fullName',
-            accessor: 'fullName',
-        },
-        {
-            Header: 'Email',
-            accessor: 'email',
-        },
-        {
-            Header: 'Last Online',
-            id: 'lastOnline',
-            accessor: (row) => formatTime(row.lastOnline),
-        },
-        {
-            Header: 'Admin',
-            id: 'admin',
-            accessor: (row, _rowIndex) => String(row.admin) // hack needed for boolean values to show up
-        },
-        {
-            Header: 'Instructor',
-            id: 'instructor',
-            accessor: (row, _rowIndex) => String(row.instructor) // hack needed for boolean values to show up
-        },
-    ];
+const columns = [
+    {
+        Header: 'githubId',
+        accessor: 'githubId', // accessor is the "key" in the data
+    },
+    {
+        Header: 'githubLogin',
+        accessor: 'githubLogin', // accessor is the "key" in the data
+    },
+    {
+        Header: 'fullName',
+        accessor: 'fullName',
+    },
+    {
+        Header: 'Email',
+        accessor: 'email',
+    },
+    {
+        Header: 'Last Online',
+        id: 'lastOnline',
+        accessor: (row) => formatTime(row.lastOnline),
+    },
+    {
+        Header: 'Admin',
+        id: 'admin',
+        accessor: (row, _rowIndex) => String(row.admin) // hack needed for boolean values to show up
+    },
+    {
+        Header: 'Instructor',
+        id: 'instructor',
+        accessor: (row, _rowIndex) => String(row.instructor) // hack needed for boolean values to show up
+    },
+];
 
-export default function UsersTable({ users, showToggleButtons}) {
+export default function UsersTable({ users, showToggleButtons = true}) {
     const POST = "POST"
     const usr = "/api/admin/users/"
     function cellToAxiosParamsToggleAdmin(cell) {
