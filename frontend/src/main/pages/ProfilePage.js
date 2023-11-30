783bbaece694c84d11d66b790c66a99632afdf75
cstraw2021 <86491289+cstraw2021@users.noreply.github.com>
2023-11-30 13:21:07 -0800

    convert to timeString

--- frontend/src/main/pages/ProfilePage.js
+++ frontend/src/main/pages/ProfilePage.js
@@ -5,27 +5,28 @@ import UsersTable from "main/components/Users/UsersTable";
 import UserEmailsTable from "main/components/Users/UserEmailsTable";
 import { useUsers } from "main/utils/users";
 import { useCurrentUser } from "main/utils/currentUser";
+import { timestampToDate } from "main/utils/dateUtils";
+
 const ProfilePage = () => {
 
-    const { data: currUser } = useUsers();
+    const { data: currUser} = useUsers();
     const { data: currentUser } = useCurrentUser();
 
-    // Check if the user is not logged in
     if (!currentUser.loggedIn) {
         return (
             <p>Not logged in.</p>
         )
     }
-
-    // Find the user in currUser array with matching githubId
-    const foundUser = currUser.find(user => user.githubId === currentUser.root.user.githubId);
-
-    // If a user with matching githubId is found, update displayUser's lastOnline
-    const displayUser = { ...currentUser };
-    if (foundUser) {
-        displayUser.root.user.lastOnline = foundUser.lastOnline;
-    }
-
+    const displayUser = {
+        ...currentUser,
+        root: {
+          ...currentUser.root,
+          user: {
+            ...currentUser.root.user,
+            lastOnline: timestampToDate(currUser.lastOnline)
+          }
+        }
+      };
 
     return (
         <BasicLayout>
