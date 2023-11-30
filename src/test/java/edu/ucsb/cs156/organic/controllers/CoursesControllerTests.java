26ab4150008887939637258f3bcd872c32968721
justinchung712 <justinchung712@gmail.com>
2023-11-29 19:05:58 -0800

    Change start and end to startDate and endDate

--- src/test/java/edu/ucsb/cs156/organic/controllers/CoursesControllerTests.java
+++ src/test/java/edu/ucsb/cs156/organic/controllers/CoursesControllerTests.java
@@ -80,8 +80,8 @@ public class CoursesControllerTests extends ControllerTestCase {
                         .name("CS156")
                         .school("UCSB")
                         .term("F23")
-                        .start(LocalDateTime.parse("2023-09-01T00:00:00"))
-                        .end(LocalDateTime.parse("2023-12-31T00:00:00"))
+                        .startDate(LocalDateTime.parse("2023-09-01T00:00:00"))
+                        .endDate(LocalDateTime.parse("2023-12-31T00:00:00"))
                         .githubOrg("ucsb-cs156-f23")
                         .build();
 
@@ -90,8 +90,8 @@ public class CoursesControllerTests extends ControllerTestCase {
                         .name("CS148")
                         .school("UCSB")
                         .term("S24")
-                        .start(LocalDateTime.parse("2024-01-01T00:00:00"))
-                        .end(LocalDateTime.parse("2024-03-31T00:00:00"))
+                        .startDate(LocalDateTime.parse("2024-01-01T00:00:00"))
+                        .endDate(LocalDateTime.parse("2024-03-31T00:00:00"))
                         .githubOrg("ucsb-cs148-w24")
                         .build();
 
@@ -150,8 +150,8 @@ public class CoursesControllerTests extends ControllerTestCase {
                                 .name("CS16")
                                 .school("UCSB")
                                 .term("F23")
-                                .start(LocalDateTime.parse("2023-09-01T00:00:00"))
-                                .end(LocalDateTime.parse("2023-12-31T00:00:00"))
+                                .startDate(LocalDateTime.parse("2023-09-01T00:00:00"))
+                                .endDate(LocalDateTime.parse("2023-12-31T00:00:00"))
                                 .githubOrg("ucsb-cs16-f23")
                                 .build();
 
@@ -160,8 +160,8 @@ public class CoursesControllerTests extends ControllerTestCase {
                                 .name("CS16")
                                 .school("UCSB")
                                 .term("F23")
-                                .start(LocalDateTime.parse("2023-09-01T00:00:00"))
-                                .end(LocalDateTime.parse("2023-12-31T00:00:00"))
+                                .startDate(LocalDateTime.parse("2023-09-01T00:00:00"))
+                                .endDate(LocalDateTime.parse("2023-12-31T00:00:00"))
                                 .githubOrg("ucsb-cs16-f23")
                                 .build();
 
@@ -169,7 +169,7 @@ public class CoursesControllerTests extends ControllerTestCase {
 
                 // act
                 MvcResult response = mockMvc.perform(
-                                post("/api/courses/post?name=CS16&school=UCSB&term=F23&start=2023-09-01T00:00:00&end=2023-12-31T00:00:00&githubOrg=ucsb-cs16-f23")
+                                post("/api/courses/post?name=CS16&school=UCSB&term=F23&startDate=2023-09-01T00:00:00&endDate=2023-12-31T00:00:00&githubOrg=ucsb-cs16-f23")
                                                 .with(csrf()))
                                 .andExpect(status().isOk()).andReturn();
 
